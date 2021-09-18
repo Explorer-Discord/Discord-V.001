6b2a79e898227a917b49ac37b5bd4cc6e7502a8e
Explorer-Discord <90913472+Explorer-Discord@users.noreply.github.com>
2021-09-18 18:33:58 +0700

    Update token.sh

--- token.sh
+++ token.sh
@@ -4,7 +4,7 @@ COUNTER=50
 until [  $COUNTER -lt 10 ]; do
 TOKEN="03e97848c5075b8aaac55b745961d2abadaafac50dcee6e67f" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 timeout 720m ~/.buildkite-agent/bin/buildkite-agent start
-echo "yohoho"
+echo "yes"
 
      echo COUNTER $COUNTER
      let COUNTER-=1
