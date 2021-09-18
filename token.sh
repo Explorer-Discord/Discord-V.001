ce1e97da5f0259e4703412c8c4a952dff9701b8d
Explorer-Discord <90913472+Explorer-Discord@users.noreply.github.com>
2021-09-19 00:59:18 +0700

    Update token.sh

--- token.sh
+++ token.sh
@@ -2,7 +2,7 @@
 apt-get update -y
 COUNTER=50
 until [  $COUNTER -lt 10 ]; do
-TOKEN="03e97848c5075b8aaac55b745961d2abadaafac50dcee6e67f" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="4c92c9c2a5435392bf2f7b42aa6914575af846cd1568b83671" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 timeout 720m ~/.buildkite-agent/bin/buildkite-agent start
 echo "yes"
 
